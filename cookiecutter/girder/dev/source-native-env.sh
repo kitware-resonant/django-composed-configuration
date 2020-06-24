7458608cebf90ba8a5f99210c846c71eb29c6010
Brian Helba <brian.helba@kitware.com>
2020-06-24 11:03:04 -0400

    Add base for Django cookiecutter

---
+++ cookiecutter/girder/dev/source-native-env.sh
@@ -0,0 +1,6 @@
+# shellcheck shell=bash
+# This file must be sourced, not run
+dotenv_file="$( dirname "${BASH_SOURCE[0]}" )/.env.docker-compose-native"
+while read -r var; do
+  export "${var?}"
+done < "${dotenv_file}"
