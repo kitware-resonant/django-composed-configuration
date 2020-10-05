e59ad50c1db8def928b2b62560246be5ba608f2e
Brian Helba <brian.helba@kitware.com>
2020-10-05 11:01:23 -0400

    Move configuration files to the top level

---
+++ composed_configuration/__init__.py
@@ -0,0 +1,45 @@
+from ._base import ComposedConfiguration, ConfigMixin
+from ._celery import CeleryMixin
+from ._configuration import (
+    DevelopmentBaseConfiguration,
+    HerokuProductionBaseConfiguration,
+    ProductionBaseConfiguration,
+    TestingBaseConfiguration,
+)
+from ._cors import CorsMixin
+from ._database import DatabaseMixin
+from ._debug import DebugMixin
+from ._django import DjangoMixin
+from ._email import ConsoleEmailMixin, SmtpEmailMixin
+from ._extensions import ExtensionsMixin
+from ._filter import FilterMixin
+from ._https import HttpsMixin
+from ._logging import LoggingMixin
+from ._rest_framework import RestFrameworkMixin
+from ._static import StaticFileMixin, WhitenoiseStaticFileMixin
+from ._storage import MinioStorageMixin, S3StorageMixin
+
+__all__ = [
+    'CeleryMixin',
+    'ComposedConfiguration',
+    'ConfigMixin',
+    'ConsoleEmailMixin',
+    'CorsMixin',
+    'DatabaseMixin',
+    'DebugMixin',
+    'DevelopmentBaseConfiguration',
+    'DjangoMixin',
+    'ExtensionsMixin',
+    'FilterMixin',
+    'HerokuProductionBaseConfiguration',
+    'HttpsMixin',
+    'LoggingMixin',
+    'MinioStorageMixin',
+    'ProductionBaseConfiguration',
+    'RestFrameworkMixin',
+    'S3StorageMixin',
+    'SmtpEmailMixin',
+    'StaticFileMixin',
+    'TestingBaseConfiguration',
+    'WhitenoiseStaticFileMixin',
+]
