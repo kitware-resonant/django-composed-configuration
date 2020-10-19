9dabb4caddb2fc71e57b266ccb1735ad1a122d71
Brian Helba <brian.helba@kitware.com>
2020-10-19 16:18:14 -0400

    Configure Sentry via an app
    This results in cleaner settings and a more stable start-up process.

--- composed_configuration/_sentry.py
+++ composed_configuration/_sentry.py
@@ -1,4 +1,3 @@
-import logging
 from typing import Type
 
 from configurations import values
@@ -12,47 +11,20 @@ class SentryConfig(ConfigMixin):
 
     This requires the `sentry-sdk` package to be installed.
 
-    The `SENTRY_DSN` environment variable should be externally set to a Sentry DSN.
-    """
+    The `DJANGO_SENTRY_DSN` environment variable should be externally set to a Sentry DSN.
 
-    # Sentry documents this and other environment variables (SENTRY_RELEASE, etc.) to have no
-    # DJANGO_ prefix, so follow that here.
-    # Disable late_binding, so the value is available immediately in before_binding.
-    SENTRY_DSN = values.Value(None, environ_prefix=None, late_binding=False)
+    The `DJANGO_SENTRY_ENVIRONMENT` and `DJANGO_SENTRY_RELEASE` environment variables may also
+    be set, if desired.
+    """
 
     @staticmethod
     def before_binding(configuration: Type[ComposedConfiguration]) -> None:
-        try:
-            import sentry_sdk
-        except ImportError:
-            # If "sentry-sdk" is not installed, do not proceed
-            return
-
-        # These should always succeed, if the package is installed
-        from sentry_sdk.integrations.django import DjangoIntegration
-        from sentry_sdk.integrations.logging import LoggingIntegration
-
-        integrations = [
-            DjangoIntegration(),
-            LoggingIntegration(level=logging.INFO, event_level=logging.WARNING),
+        configuration.INSTALLED_APPS += [
+            'composed_configuration.sentry.apps.SentryConfig',
         ]
 
-        try:
-            from sentry_sdk.integrations.celery import CeleryIntegration
-        except sentry_sdk.integrations.DidNotEnable:
-            # If "celery" is not installed, we can still proceed without that integration
-            pass
-        else:
-            integrations.append(CeleryIntegration())
+    SENTRY_DSN = values.Value(None)
 
-        sentry_sdk.init(
-            # Even if a "dsn" is not explicitly passed, sentry_sdk will attempt to find the DSN in
-            # the SENTRY_DSN environment variable; however, by making it an explicit setting,
-            # it can be overridden by subclasses
-            dsn=configuration.SENTRY_DSN,
-            integrations=integrations,
-            # Send traces for non-exception events too
-            attach_stacktrace=True,
-            # Submit request User info from Django
-            send_default_pii=True,
-        )
+    SENTRY_ENVIRONMENT = values.Value(None)
+
+    SENTRY_RELEASE = values.Value(None)
