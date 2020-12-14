93aae775ec95f99ededaa4e24f139da1310688c5
Brian Helba <brian.helba@kitware.com>
2020-12-14 10:59:57 -0500

    Add a package classifier for Python 3.9 support

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python',
     ],
     python_requires='>=3.8',
