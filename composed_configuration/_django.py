855dee720314c85210951382a51ef345385599d3
Roni Choudhury <2903332+waxlamp@users.noreply.github.com>
2021-06-18 14:59:16 -0400

    Set Eastern Time as the default timezone

--- composed_configuration/_django.py
+++ composed_configuration/_django.py
@@ -85,6 +85,6 @@ class DjangoMixin(ConfigMixin):
     # https://docs.djangoproject.com/en/3.0/topics/i18n/
     LANGUAGE_CODE = 'en-us'
     USE_TZ = True
-    TIME_ZONE = 'UTC'
+    TIME_ZONE = 'America/New_York'
     USE_I18N = True
     USE_L10N = True
