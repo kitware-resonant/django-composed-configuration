c01e55e366d335029875e08b3804d75b31f78637
Dan LaManna <dan.lamanna@kitware.com>
2021-11-11 11:58:38 -0500

    Disable PRETTIFY_SQL in Django debug toolbar

--- composed_configuration/_debug.py
+++ composed_configuration/_debug.py
@@ -22,4 +22,6 @@ class DebugMixin(ConfigMixin):
     DEBUG_TOOLBAR_CONFIG = {
         # The default size often is too small, causing an inability to view queries
         'RESULTS_CACHE_SIZE': 250,
+        # If this setting is True, large sql queries can cause the page to render slowly
+        'PRETTIFY_SQL': False,
     }
