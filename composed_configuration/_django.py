c33feed405eef1373945a210ff1bf5365f9c3f89
Brian Helba <brian.helba@kitware.com>
2022-04-04 16:07:58 -0400

    Define the full list of PASSWORD_HASHERS
    This is more straightforward to understand and override downstream.

--- composed_configuration/_django.py
+++ composed_configuration/_django.py
@@ -41,9 +41,6 @@ class DjangoMixin(ConfigMixin):
             'django.middleware.clickjacking.XFrameOptionsMiddleware',
         ]
 
-        # Scrypt needs to be added to the beginning of the list to support older hashers
-        configuration.PASSWORD_HASHERS.insert(0, 'django.contrib.auth.hashers.ScryptPasswordHasher')
-
     SECRET_KEY = values.SecretValue()
     ALLOWED_HOSTS = values.ListValue(environ_required=True)
 
@@ -73,7 +70,15 @@ class DjangoMixin(ConfigMixin):
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-    # Password validation
+    # Make scrypt the default hasher. It must be explicitly added in Django 4:
+    # https://docs.djangoproject.com/en/4.0/topics/auth/passwords/#scrypt-usage
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.ScryptPasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
     # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
     AUTH_PASSWORD_VALIDATORS = [
         {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
